040fee1a357a33c7fdb95892cf820a55dd713619
Šarūnas Ramonas <sarunas.ramonas@gmail.com>
2023-01-22 14:43:07 +0200

    - Update selection feature
    Update material of selection mesh + selection function update.

---
+++ Source/YKAE/CoreBPFunctionLibrary.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreBPFunctionLibrary.h"
+
+#include "Engine/TextureRenderTarget2D.h"
+
+void UCoreBPFunctionLibrary::ConvertRenderTargetToTexture(UTextureRenderTarget2D* RenderTarget,
+                                                          UTexture2D*& ExportedTexture)
+{
+}
